29f709bb94a08201e78e0e2748805b3cd9ce6f47
temelehtone <91484193+temelehtone@users.noreply.github.com>
2022-05-21 14:31:00 +0300

    productModel UI ready

--- client/src/components/AdminDashboard.js
+++ client/src/components/AdminDashboard.js
@@ -3,7 +3,19 @@ import isEmpty from "validator/lib/isEmpty";
 import DashboardIcon from "@mui/icons-material/Dashboard";
 import AddIcon from "@mui/icons-material/Add";
 import CreditCardIcon from "@mui/icons-material/CreditCard";
-import { Box, Button, Modal, Typography, TextField, LinearProgress } from "@mui/material";
+import {
+  Box,
+  Button,
+  Modal,
+  Typography,
+  TextField,
+  LinearProgress,
+  Input,
+  Grid,
+  Select,
+  InputLabel,
+  MenuItem,
+} from "@mui/material";
 import { ThemeProvider, createTheme, styled } from "@mui/material/styles";
 
 import { red, blue, green } from "@mui/material/colors";
@@ -32,14 +44,15 @@ const FlexBox = styled(Box)(({ theme }) => ({
 }));
 
 const ButtonBox = styled(Box)(({ theme }) => ({
-  display: "flex", 
+  display: "flex",
   width: "100%",
   justifyContent: "space-between",
   paddingInline: "20px",
+  borderTop: "1px solid #777",
   [theme.breakpoints.down("sm")]: {
-    paddingInline: 10
+    paddingInline: 10,
   },
-}))
+}));
 
 const CustomModal = styled(Modal)(({ theme }) => ({
   width: "50%",
@@ -52,18 +65,28 @@ const CustomModal = styled(Modal)(({ theme }) => ({
   transform: `translate(-${50}%, -${50}%)`,
   [theme.breakpoints.down("sm")]: {
     width: "70%",
-    height: 400,
   },
-}))
-const StyledTextField = styled(TextField)(({ theme }) => ({
-    margin: "20px",
-    
+}));
+const CustomProductModal = styled(Modal)(({ theme }) => ({
+  width: "50%",
+  display: "flex",
+  justifyContent: "center",
+  border: "2px solid #000",
+  left: "50%",
+  top: "50%",
+  transform: `translate(-${50}%, -${50}%)`,
+  overflowY: "auto",
+  backgroundColor: "#fff",
+  [theme.breakpoints.down("sm")]: {
+    width: "70%",
+  },
 }));
 
-
+const StyledTextField = styled(TextField)(({ theme }) => ({
+  margin: "20px",
+}));
 
 const AdminDashboard = () => {
-  
   const theme = createTheme({
     palette: {
       secondary: {
@@ -78,146 +101,399 @@ const AdminDashboard = () => {
     },
   });
 
+  const initialProductValues = {
+    image: "",
+    name: "",
+    description: "",
+    price: "",
+    productCategory: "Choose One...",
+    quantity: 0,
+  }
+
   const [alert, setAlert] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  const [category, setCategory] = useState('')
+  const [category, setCategory] = useState("");
+  const [productData, setProductData] = useState(initialProductValues);
 
   const [openCategory, setOpenCategory] = useState(false);
-  const [openItem, setOpenItem] = useState(false);
+  const [openProduct, setOpenProduct] = useState(false);
   const [openOrders, setOpenOrders] = useState(false);
+
+  
 
   const handleOpenCategory = () => {
     setOpenCategory(true);
-  }
+  };
   const handleCategoryClose = () => {
     setOpenCategory(false);
-    setCategory("")
-    setAlert(false)
+    setCategory("");
+    setAlert(false);
   };
-  const handleOpenItem = () => {
-    setOpenItem(true);
-  }
-  const handleItemClose = () => {
-    setOpenItem(false);
+  const handleOpenProduct = () => {
+    setOpenProduct(true);
+  };
+  const handleProductClose = () => {
+    setOpenProduct(false);
   };
   const handleOpenOrders = () => {
     setOpenOrders(true);
-  }
+  };
   const handleOrdersClose = () => {
     setOpenOrders(false);
   };
 
   const handleCategoryChange = (e) => {
     setCategory(e.target.value);
-  }
+  };
+
+  const handleProductChange = (e) => {
+    setProductData({ ...productData, [e.target.name]: e.target.value });
+  };
+
   const handleCategorySubmit = async (e) => {
     e.preventDefault();
     if (isEmpty(category)) {
       setAlert({
         severity: "error",
-        message: "Category can't be empty."
-      })
-      return
+        message: "Category can't be empty.",
+      });
+      return;
     }
-    const data = { category }
+    const data = { category };
     setLoading(true);
-    await createCategory(data, setAlert)
+    await createCategory(data, setAlert);
     setLoading(false);
+  };
+
+  const handleProductSubmit = async (e) => {
+    e.preventDefault();
+    console.log(productData);
   }
 
+
   const categoryModal = (
-      <CustomModal
-        open={openCategory}
-        onClose={handleCategoryClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
+    <CustomModal
+      open={openCategory}
+      onClose={handleCategoryClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box
+        sx={{
+          display: "flex",
+          width: "100%",
+          bgcolor: "white",
+          flexDirection: "column",
+        }}
+        onSubmit={handleCategorySubmit}
+        component="form"
       >
-        <Box sx={{ display: "flex", width: "100%", bgcolor: "white", flexDirection: "column" }} onSubmit={handleCategorySubmit} component="form">
-        
-          
-          {alert ? <AlertDismissible {...alert} setAlert={setAlert} /> : 
-          <Box sx={{ bgcolor: "secondary.green", width: "100%", display: "flex", alignItems: "center", height: "100px" }} >
-          <Typography id="modal-modal-title" variant="h6" component="h4" sx={{ ml: 3, color: "#fff" }}>
-            Add Category
-          </Typography>
-          
-        </Box>}
-          {loading ? <LinearProgress color="primary" /> : 
+        {alert ? (
+          <AlertDismissible {...alert} setAlert={setAlert} />
+        ) : (
+          <Box
+            sx={{
+              bgcolor: "secondary.green",
+              width: "100%",
+              display: "flex",
+              alignItems: "center",
+              height: "100px",
+            }}
+          >
+            <Typography
+              id="modal-modal-title"
+              variant="h6"
+              component="h4"
+              sx={{ ml: 3, color: "#fff" }}
+            >
+              Add Category
+            </Typography>
+          </Box>
+        )}
+        {loading ? (
+          <LinearProgress color="primary" />
+        ) : (
           <>
-          <Typography sx={{ ml: 3, mt: 3}}>
-            Category
-          </Typography>
-          <StyledTextField onChange={handleCategoryChange} name="category" value={category} />
-          <ButtonBox>
-          <Button sx={{ bgcolor: "#8E8E8E", color: "white", width: "100px", my: 2, "&:hover": { bgcolor: "red"} }} onClick={handleCategoryClose}>
-            Close
-          </Button>
-          <Button sx={{ bgcolor: "secondary.green", color: "white", width: "100px", my: 2, "&:hover": { bgcolor: "secondary.blue"} }} type="submit">
-            Submit
-          </Button>
-          </ButtonBox>
-          </>}
-        </Box>
-      </CustomModal>
-    
+            <Typography sx={{ ml: 3, mt: 3 }}>Category</Typography>
+            <StyledTextField
+              onChange={handleCategoryChange}
+              name="category"
+              value={category}
+            />
+            <ButtonBox>
+              <Button
+                sx={{
+                  bgcolor: "#8E8E8E",
+                  color: "white",
+                  width: "100px",
+                  my: 2,
+                  "&:hover": { bgcolor: "red" },
+                }}
+                onClick={handleCategoryClose}
+              >
+                Close
+              </Button>
+              <Button
+                sx={{
+                  bgcolor: "secondary.green",
+                  color: "white",
+                  width: "100px",
+                  my: 2,
+                  "&:hover": { bgcolor: "secondary.blue" },
+                }}
+                type="submit"
+              >
+                Submit
+              </Button>
+            </ButtonBox>
+          </>
+        )}
+      </Box>
+    </CustomModal>
   );
-  const itemModal = (
-      <CustomModal
-        open={openItem}
-        onClose={handleItemClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
+
+ const categories = ["Drinks", "Meat", "Desserts"]
+
+  const productModal = (
+    <CustomProductModal
+      open={openProduct}
+      onClose={handleProductClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box
+        sx={{
+          display: "flex",
+          width: "100%",
+          bgcolor: "white",
+          flexDirection: "column",
+        }}
       >
-        <Box sx={{ display: "flex", width: "100%", bgcolor: "white", flexDirection: "column" }}>
-        <Box sx={{ bgcolor: "secondary.blue", width: "100%", display: "flex", alignItems: "center", height: "30%" }}>
-          <Typography id="modal-modal-title" variant="h6" component="h4" sx={{ ml: 3, color: "#fff" }}>
-            Add Item
-          </Typography>
-        </Box>
-          <Typography sx={{ ml: 3, mt: 3}}>
-            Item
-          </Typography>
-          <StyledTextField />
-          <ButtonBox>
-          <Button sx={{ bgcolor: "#8E8E8E", color: "white", width: "100px", my: 2, "&:hover": { bgcolor: "red"} }} onClick={handleItemClose}>
-            Close
-          </Button>
-          <Button sx={{ bgcolor: "secondary.blue", color: "white", width: "100px", mx: 4, my: 2, "&:hover": { bgcolor: "secondary.blueHover"} }}>
-            Submit
-          </Button>
-          </ButtonBox>
-          
-        </Box>
-      </CustomModal>
-    
+        {alert ? (
+          <AlertDismissible {...alert} setAlert={setAlert} />
+        ) : (
+          <Box
+            sx={{
+              bgcolor: "secondary.blue",
+              width: "100%",
+              display: "flex",
+              alignItems: "center",
+              height: "200px",
+            }}
+          >
+            <Typography
+              id="modal-modal-title"
+              variant="h6"
+              component="h4"
+              sx={{ ml: 3, color: "#fff" }}
+            >
+              Add Product
+            </Typography>
+          </Box>
+        )}
+        {loading ? (
+          <LinearProgress color="primary" />
+        ) : (
+          <>
+            <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+              <Grid item xs={11} sm={11} sx={{ ml: 1}}>
+              <InputLabel htmlFor="image" >Image</InputLabel>
+                <Input
+                  accept="image/*"
+                  style={{ display: "none" }}
+                  id="image"
+                  multiple
+                  type="file"
+                />
+                <label
+                  htmlFor="image"
+                  style={{
+                    display: "flex",
+                    justifyContent: "flex-end",
+                    border: "1px solid",
+                    backgroundColor: "#F2F2F2",
+                  }}
+                >
+                  <Button
+                    variant="raised"
+                    component="span"
+                    sx={{
+                      bgcolor: "secondary.blue",
+                      "&:hover": { bgcolor: "secondary.blueHover" },
+                    }}
+                  >
+                    Browse
+                  </Button>
+                </label>
+              </Grid>
+              <Grid item xs={11} sx={{ ml: 1}}>
+              <InputLabel htmlFor="name" >Name</InputLabel>
+                <TextField
+                  onChange={handleProductChange}
+                  fullWidth
+                  id="name"
+                  name="name"
+                />
+              </Grid>
+              <Grid
+                item
+                xs={11}
+                sx={{ display: "flex", flexDirection: "column", ml: 1 }}
+              >
+                <InputLabel htmlFor="description" >Description</InputLabel>
+                <textarea
+                  onChange={handleProductChange}
+                  id="description"
+                  name="description"
+                  style={{ width: "100%", resize: "vertical", height: 100}}
+                />
+              </Grid>
+              <Grid item xs={11} sx={{ ml: 1}}>
+              <InputLabel htmlFor="price" >Price</InputLabel>
+                <TextField
+                  onChange={handleProductChange}
+                  fullWidth
+                  name="price"
+                  type="price"
+                  id="price"
+                />
+              </Grid>
+              <Grid item xs={11} sm={6} sx={{ ml: 1}}>
+                <InputLabel htmlFor="productCategory" >Category</InputLabel>
+                <Select
+                  labelId="productCategory"
+                  id="productCategory"
+                  value={productData.productCategory}
+                  onChange={handleProductChange}
+                  fullWidth
+                  name="productCategory"
+                  displayEmpty
+                >
+                  <MenuItem disabled value="Choose One...">Choose One...</MenuItem>
+                  {categories.map((cat) => (
+                    <MenuItem 
+                    key={cat}
+                    value={cat}>
+                      {cat}
+                      </MenuItem>
+                  ))}
+  
+                </Select>
+              </Grid>
+              <Grid item xs={11} sm={5} sx={[ (theme) => ({
+                [theme.breakpoints.down("sm")]: {
+                  ml: 1,
+                }
+              }) ]}>
+              <InputLabel htmlFor="quantity" >Quantity</InputLabel>
+                <TextField
+                  onChange={handleProductChange}
+                  id="quantity"
+                  name="quantity"
+                  type="number"
+                  fullWidth
+                  InputProps={{ inputProps: { min: 0, max: 1000 }}}
+                  value={productData.quantity}
+                  
+                />
+              </Grid>
+            </Grid>
+
+            <ButtonBox component="form" onSubmit={handleProductSubmit}>
+              <Button
+                sx={{
+                  bgcolor: "#8E8E8E",
+                  color: "white",
+                  width: "100px",
+                  my: 2,
+                  "&:hover": { bgcolor: "red" },
+                }}
+                onClick={handleProductClose}
+              >
+                Close
+              </Button>
+              <Button
+                sx={{
+                  bgcolor: "secondary.blue",
+                  color: "white",
+                  width: "100px",
+                  mx: 4,
+                  my: 2,
+                  "&:hover": { bgcolor: "secondary.blueHover" },
+                }}
+                type="submit"
+              >
+                Submit
+              </Button>
+            </ButtonBox>
+          </>
+        )}
+      </Box>
+    </CustomProductModal>
   );
   const ordersModal = (
-      <CustomModal
-        open={openOrders}
-        onClose={handleOrdersClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
+    <CustomModal
+      open={openOrders}
+      onClose={handleOrdersClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box
+        sx={{
+          display: "flex",
+          width: "100%",
+          bgcolor: "white",
+          flexDirection: "column",
+        }}
       >
-        <Box sx={{ display: "flex", width: "100%", bgcolor: "white", flexDirection: "column" }}>
-        <Box sx={{ bgcolor: "secondary.red", width: "100%", display: "flex", alignItems: "center", height: "30%" }}>
-          <Typography id="modal-modal-title" variant="h6" component="h4" sx={{ ml: 3, color: "#fff" }}>
+        <Box
+          sx={{
+            bgcolor: "secondary.red",
+            width: "100%",
+            display: "flex",
+            alignItems: "center",
+            height: "30%",
+          }}
+        >
+          <Typography
+            id="modal-modal-title"
+            variant="h6"
+            component="h4"
+            sx={{ ml: 3, color: "#fff" }}
+          >
             Orders
           </Typography>
         </Box>
-          <StyledTextField />
-          <ButtonBox>
-          <Button sx={{ bgcolor: "#8E8E8E", color: "white", width: "100px", my: 2, "&:hover": { bgcolor: "red"} }} onClick={handleOrdersClose}>
+        <StyledTextField />
+        <ButtonBox>
+          <Button
+            sx={{
+              bgcolor: "#8E8E8E",
+              color: "white",
+              width: "100px",
+              my: 2,
+              "&:hover": { bgcolor: "red" },
+            }}
+            onClick={handleOrdersClose}
+          >
             Close
           </Button>
-          <Button sx={{ bgcolor: "secondary.green", color: "white", width: "100px", mx: 4, my: 2, "&:hover": { bgcolor: "secondary.blue"} }}>
+          <Button
+            sx={{
+              bgcolor: "secondary.green",
+              color: "white",
+              width: "100px",
+              mx: 4,
+              my: 2,
+              "&:hover": { bgcolor: "secondary.blue" },
+            }}
+          >
             Submit
           </Button>
-          </ButtonBox>
-          
-        </Box>
-      </CustomModal>
-    
+        </ButtonBox>
+      </Box>
+    </CustomModal>
   );
 
   return (
@@ -244,10 +520,10 @@ const AdminDashboard = () => {
             borderColor: "secondary.blue",
             "&:hover": { bgcolor: "secondary.blueHover" },
           }}
-          onClick={handleOpenItem}
+          onClick={handleOpenProduct}
         >
           <AddIcon />
-          Add Item
+          Add Product
         </StyledButton>
         <StyledButton
           sx={{
@@ -261,12 +537,10 @@ const AdminDashboard = () => {
           View Orders
         </StyledButton>
       </FlexBox>
-      
+
       {categoryModal}
-      {itemModal}
+      {productModal}
       {ordersModal}
-      
-      
     </ThemeProvider>
   );
 };
